b0c7b88cac6adfa45956a79ecec3381ce13e6aae
Valci Júnior <valcijunior19@hotmail.com>
2021-04-22 22:32:22 -0300

    implementation class PeronMapper

--- src/main/java/com/personapi/controller/PersonController.java
+++ src/main/java/com/personapi/controller/PersonController.java
@@ -1,14 +1,14 @@
 package com.personapi.controller;
 
+import com.personapi.dto.request.PersonDTO;
 import com.personapi.dto.response.MessageRespondeDTO;
 import com.personapi.entity.Person;
 import com.personapi.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 
 @RestController
@@ -24,7 +24,7 @@ public class PersonController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public MessageRespondeDTO createPerson(Person person){
-        return personService.createPerson(person);
+    public MessageRespondeDTO createPerson(@RequestBody @Valid PersonDTO personDTO){
+        return personService.createPerson(personDTO);
     }
 }
--- src/main/java/com/personapi/service/PersonService.java
+++ src/main/java/com/personapi/service/PersonService.java
@@ -1,17 +1,21 @@
 package com.personapi.service;
 
+import com.personapi.controller.PersonController;
+import com.personapi.dto.request.PersonDTO;
 import com.personapi.dto.response.MessageRespondeDTO;
 import com.personapi.entity.Person;
+import com.personapi.mapper.PersonMapper;
 import com.personapi.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @Service
 public class PersonService {
 
     private PersonRepository personRepository;
+
+    private PersonMapper personMapper = PersonMapper.INSTANCE;
 
     @Autowired
     public PersonService(PersonRepository personRepository) {
@@ -19,8 +23,10 @@ public class PersonService {
     }
 
     @PostMapping
-    public MessageRespondeDTO createPerson(Person person){
-        Person personSaved = personRepository.save(person);
+    public MessageRespondeDTO createPerson(PersonDTO personDTO){
+        Person personToSaved = personMapper.toModel(personDTO);
+        
+        Person personSaved = personRepository.save(personToSaved);
         return MessageRespondeDTO
                 .builder()
                 .message("Created person with ID " + personSaved.getId())
